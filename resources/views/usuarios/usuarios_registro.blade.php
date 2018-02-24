ff0b338fce4b4d82e2287b292271a895798dbf6a
DiegoCalvillo <diegocalvillordz@gmail.com>
2018-02-24 00:06:39 -0600

    Se empieza con el formulario para agregar nuevos usuarios

--- resources/views/usuarios/usuarios_registro.blade.php
+++ resources/views/usuarios/usuarios_registro.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.principal')
 
 @section('content_usuarios_registro')
-
+<div class="content-wrapper">
+	<div class="container-fluid">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item">
+				<a href="/">Inicio</a>
+			</li>
+			<li class="breadcrumb-item">
+				<a href="/usuarios">Usuarios</a>
+			</li>
+			<li class="breadcrumb-item active">Registro de Usuarios</li>
+		</ol>
+		<div class="container">
+			<div class="card card-register mx-auto mt-5">
+				<div class="card-header">
+					Registro de Usuarios
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @stop
